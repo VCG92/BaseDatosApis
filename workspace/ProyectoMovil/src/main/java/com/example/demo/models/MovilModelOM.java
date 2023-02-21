e1178a36998581ac099cbf417e11501990c490c5
Victor C&G <vcastanedag02@iesarroyoharnina.es>
2023-02-21 13:33:01 +0100

    controller service

--- workspace/ProyectoMovil/src/main/java/com/example/demo/models/MovilModelOM.java
+++ workspace/ProyectoMovil/src/main/java/com/example/demo/models/MovilModelOM.java
@@ -1,10 +1,12 @@
 package com.example.demo.models;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MovilModelOM {
 	
-	public MovilModel getMoviles(){
+	public MovilModel getMovil(){
 	 MovilModel om = new MovilModel("Samsung"
 			, new MarcaModel("Galaxy S5")
 			, new ProcesadorModel("Tiger Lake", 8, 3.2f)
@@ -15,13 +17,61 @@ public class MovilModelOM {
 	return om;
 	}
 	
-//	public static MovilModel getMoviles(){
-//		MarcaModel modelo = new MarcaModel("Galaxy S5");
-//		ProcesadorModel procesador =  new ProcesadorModel("Tiger Lake", 8, 3.2f);
-//		PantallaModel pantalla = new PantallaModel(7.55f, "OLED");
-//		
-//		MovilModel om = new MovilModel("Samsung", modelo, procesador, pantalla, 64, 8, 10, 4, 3, 2, 48, 300, false, 256, LocalDate.now());
-//		return om;
-//	}
+	public List<MovilModel> getMoviles(){
+		List<MovilModel> moviles = new ArrayList<>();
+		MovilModel om = new MovilModel("Xiaomi"
+				, new MarcaModel("Galaxy S5")
+				, new ProcesadorModel("Tiger Lake", 8, 3.2f)
+				, new PantallaModel(7.55f, "OLED")
+				, 64, 8, 10, 4, 3, 2, 48, 300, false
+				, 256, LocalDate.now());
+		MovilModel om2 = new MovilModel("LC"
+				, new MarcaModel("Galaxy S5")
+				, new ProcesadorModel("Tiger Lake", 8, 3.2f)
+				, new PantallaModel(7.55f, "OLED")
+				, 64, 8, 10, 4, 3, 2, 48, 300, false
+				, 256, LocalDate.now());
+		MovilModel om3 = new MovilModel("Alcatel"
+				, new MarcaModel("Galaxy S5")
+				, new ProcesadorModel("Tiger Lake", 8, 3.2f)
+				, new PantallaModel(7.55f, "OLED")
+				, 64, 8, 10, 4, 3, 2, 48, 300, false
+				, 256, LocalDate.now());
+		MovilModel om4 = new MovilModel("Sony"
+				, new MarcaModel("Galaxy S5")
+				, new ProcesadorModel("Tiger Lake", 8, 3.2f)
+				, new PantallaModel(7.55f, "OLED")
+				, 64, 8, 10, 4, 3, 2, 48, 300, false
+				, 256, LocalDate.now());
+		MovilModel om5 = new MovilModel("Cubot"
+				, new MarcaModel("Galaxy S5")
+				, new ProcesadorModel("Tiger Lake", 8, 3.2f)
+				, new PantallaModel(7.55f, "OLED")
+				, 64, 8, 10, 4, 3, 2, 48, 300, false
+				, 256, LocalDate.now());
+		MovilModel om6 = new MovilModel("Iphone"
+				, new MarcaModel("Galaxy S5")
+				, new ProcesadorModel("Tiger Lake", 8, 3.2f)
+				, new PantallaModel(7.55f, "OLED")
+				, 64, 8, 10, 4, 3, 2, 48, 300, false
+				, 256, LocalDate.now());
+		MovilModel om7 = new MovilModel("MAc"
+				, new MarcaModel("Galaxy S5")
+				, new ProcesadorModel("Tiger Lake", 8, 3.2f)
+				, new PantallaModel(7.55f, "OLED")
+				, 64, 8, 10, 4, 3, 2, 48, 300, false
+				, 256, LocalDate.now());
+		
+		moviles.add(om);
+		moviles.add(om2);
+		moviles.add(om3);
+		moviles.add(om4);
+		moviles.add(om5);
+		moviles.add(om6);
+		moviles.add(om7);
+		
+		
+		return moviles;
+	}
 
 }
